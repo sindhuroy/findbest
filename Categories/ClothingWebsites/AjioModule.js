fd4391d8daf9cf17849a9a2c573d2da9686fb6c2
Sindhu Roy <sindhuroy905@gmail.com>
2023-07-08 21:40:59 +0530

    -aremove try except from clothing components

--- Categories/ClothingWebsites/AjioModule.js
+++ Categories/ClothingWebsites/AjioModule.js
@@ -5,80 +5,74 @@ const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
 const getClothesAjio = async (URL) => {
-  try {
-    const data = [];
-    const browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-  
-    await page.goto(URL);
+  const data = [];
+  const browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox'] });
+  const page = await browser.newPage();
 
-    await page.setViewport({ width: 1080, height: 1024 });
-    const elements = await page.$$(".item.rilrtl-products-list__item.item");
+  await page.goto(URL);
 
-    let minLength = 6;
-    if (minLength > elements.length) minLength = elements.length;
-    for (let i = 0; i < minLength; i++) {
-      const image = await page.evaluate(
+  await page.setViewport({ width: 1080, height: 1024 });
+  const elements = await page.$$(".item.rilrtl-products-list__item.item");
+
+  let minLength = 6;
+  if (minLength > elements.length) minLength = elements.length;
+  for (let i = 0; i < minLength; i++) {
+    const image = await page.evaluate(
+      (el) =>
+        el.querySelector("a > div > div > div > img").getAttribute("src"),
+      elements[i]
+    );
+    if (!image) continue;
+    const li = await page.evaluate(
+      (el) => el.querySelector("a").getAttribute("href"),
+      elements[i]
+    );
+    const link = `https://www.ajio.com${li}`;
+
+    const brand = await page.evaluate(
+      (el) => el.querySelector("a > div > .contentHolder > div").textContent,
+      elements[i]
+    );
+
+    const title =
+      brand +
+      " - " +
+      (await page.evaluate(
         (el) =>
-          el.querySelector("a > div > div > div > img").getAttribute("src"),
+          el.querySelector("a > div > .contentHolder > .nameCls").textContent,
         elements[i]
-      );
-      if (!image) continue;
+      ));
 
-      const li = await page.evaluate(
-        (el) => el.querySelector("a").getAttribute("href"),
-        elements[i]
-      );
-      const link = `https://www.ajio.com${li}`;
+    const discountPrice = await page.evaluate(
+      (el) =>
+        el.querySelector("a > div > .contentHolder > div > span").textContent,
+      elements[i]
+    );
 
-      const brand = await page.evaluate(
-        (el) => el.querySelector("a > div > .contentHolder > div").textContent,
-        elements[i]
-      );
+    const price = await page.evaluate(
+      (el) =>
+        el.querySelector("a > div > .contentHolder > div > div > span")
+          .textContent,
+      elements[i]
+    );
+    const discount = await page.evaluate(
+      (el) =>
+        el.querySelector("a > div > .contentHolder > div > div > .discount")
+          .textContent,
+      elements[i]
+    );
+    const scrapFrom = "Ajio";
 
-      const title =
-        brand +
-        " - " +
-        (await page.evaluate(
-          (el) =>
-            el.querySelector("a > div > .contentHolder > .nameCls").textContent,
-          elements[i]
-        ));
+    // const pordArr = prodInfo.split("₹");
+    // const title = prod
 
-      const discountPrice = await page.evaluate(
-        (el) =>
-          el.querySelector("a > div > .contentHolder > div > span").textContent,
-        elements[i]
-      );
-
-      const price = await page.evaluate(
-        (el) =>
-          el.querySelector("a > div > .contentHolder > div > div > span")
-            .textContent,
-        elements[i]
-      );
-
-      const discount = await page.evaluate(
-        (el) =>
-          el.querySelector("a > div > .contentHolder > div > div > .discount")
-            .textContent,
-        elements[i]
-      );
-      const scrapFrom = "Ajio";
-
-      // const pordArr = prodInfo.split("₹");
-      // const title = prod
-
-      data.push({ link, image, title, price, discountPrice, discount, scrapFrom });
-    }
-
-    await browser.close();
-    console.log(data.length);
-    // console.log(data);
-    return data;
-  } catch (error) {
-    console.log(error);
+    data.push({ link, image, title, price, discountPrice, discount, scrapFrom });
   }
+
+  await browser.close();
+  console.log(data.length);
+  // console.log(data);
+  return data;
 };
 const URL = "https://www.ajio.com/search/?text=jeans";
 module.exports = getClothesAjio;
